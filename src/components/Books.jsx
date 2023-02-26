183da925d888cc278fb2cc6517644344fc178d1b
Jovana Aleksic <jaleksic23@gmail.com>
2023-02-26 13:22:42 +0100

    Dodata komponenta Book

---
+++ src/components/Books.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import OneBook from "./OneBook";
+
+const Books = () => {
+  return (
+    <div>
+      <OneBook />
+      <OneBook />
+      <OneBook />
+    </div>
+  );
+};
+
+export default Books;
