e90d83146fe9ba9f069fdeed407dcd06055557cc
Jovana Aleksic <jaleksic23@gmail.com>
2023-02-26 13:22:59 +0100

    Dodata komponenta OneBook

---
+++ src/components/OneBook.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+//https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg
+const OneBook = () => {
+  return (
+    <div>
+      <div className="card mb-3" style={{ maxWidth: 540 }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg"
+              className="img-fluid rounded-start"
+              alt=""
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OneBook;
