2e09233c137bef83cfffdfe8ae306ae440e9e574
Jovana Aleksic <jaleksic23@gmail.com>
2023-02-27 14:19:15 +0100

    mapping of the books array

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import OneBook from "./OneBook";
 
+//https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg
+//https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBOPTyWGrcuwnUnRGejJbj1ARN7GpGX_XSnLkSVMvrNe7ode4E
+//https://m.media-amazon.com/images/I/615ZIxEDozL.jpg
+//https://www.booktopia.com.au/covers/600/9781647006747/0906/how-to-paint-without-a-brush.jpg
+
 const Books = ({ books }) => {
   return (
     <div className="all-books">
-      <OneBook book={books[0]} />
-      <OneBook book={books[1]} />
-      <OneBook book={books[2]} />
-      <OneBook book={books[3]} />
+      {books.map((book) => (
+        <OneBook book={book} />
+      ))}
     </div>
   );
 };
